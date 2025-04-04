92b41287b1aa37b88365d9a35d9854822cb8b375
Erik J <erenjacome@gmail.com>
2025-04-04 13:31:20 -0400

    Bringing in frontend dist folder

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 app.use(cors())
 
 ////////////////////////////////////////////////////////////////////////
